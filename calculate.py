80f7ccbdd51cebadba03a84757e8f117ca0662ff
wxfdmy20201107 <132934659+wxfdmy20201107@users.noreply.github.com>
2023-07-05 21:37:40 +0800

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from sympy import *
 
 x, y, z = symbols("x y z")
@@ -9,11 +10,14 @@ b = 96.06
 c = 110.09
 
 #请输入短链脂肪酸的量（单位为ul）
-dosis = int(input("今天预计给每只小鼠短链脂肪酸的量（单位为ul）："))
-number = int(input("今天灌小鼠的数量（单位为只）："))
-result1 = number*solve(dosis*0.001*0.001*c1*a-x*0.001,x)[0]
-result2 = number*solve(dosis*0.001*0.001*c2*b-y*0.001,y)[0]
-result3 = number*solve(dosis*0.001*0.001*c3*c-z*0.001,z)[0]
+st.header("计算小鼠短链脂肪酸的用量")
+dosis = int(st.number_input("今天预计给每只小鼠短链脂肪酸的量(单位为ul)："))
+amount = int(st.number_input("今天灌小鼠的数量(单位为只)："))
+#dosis = int(input("今天预计给每只小鼠短链脂肪酸的量(单位为ul)："))
+#number = int(input("今天灌小鼠的数量(单位为只)："))
+result1 = amount*solve(dosis*0.001*0.001*c1*a-x*0.001,x)[0]
+result2 = amount*solve(dosis*0.001*0.001*c2*b-y*0.001,y)[0]
+result3 = amount*solve(dosis*0.001*0.001*c3*c-z*0.001,z)[0]
 print("总共需要称取乙酸钠",'{:.2f}'.format(result1),"mg")
 print("总共需要称取丙酸钠",'{:.2f}'.format(result2),"mg")
 print("总共需要称取丁酸钠",'{:.2f}'.format(result3),"mg")
